a24669d32660165643d2dd8ab53f46658abd4453
Laravel Shift <shift@laravelshift.com>
2016-09-11 12:55:10 +0000

    Shift Middleware
    Laravel 5.2 adjusts the `Guard` object used within middleware. In
    addition, new `can` and `throttles` middleware were added.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,9 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
+        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
